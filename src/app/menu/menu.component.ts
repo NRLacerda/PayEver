53d676be79803e50fe3cef02544d2fb3573d1177
Nicolas <nrochalacerda@gmail.com>
2023-06-09 16:36:38 -0300

    god bless us

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,23 +9,43 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  hoursAMPM: string[] = [];
+  selectedHour: string = '';
+
+
+
   constructor(private dialog : MatDialog){
+    this.generateHoursAMPM();
   }
-  openNewAppointment(enterAnimationDuration: string, exitAnimationDuration: string, Date:Date|null): void {
-   // if(Date){
-    this.dialog.open(AppointmentComponent, {
-      width: '500px',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
-    /*}else{
-      alert("select a date");
-    }*/
+  openNewAppointment(enterAnimationDuration: string, exitAnimationDuration: string, date:string): void {
+      const dialogConfig = {
+        width: '500px',
+        data: {
+          enterAnimationDuration,
+          exitAnimationDuration,
+          date
+        },
+      }
+    this.dialog.open(AppointmentComponent, dialogConfig);
   }
-  onDateSelected(event:any):void {
-    // Your logic with the selected date
-    console.log('Selected date:', event);
-    // Call your function here with the selected date as a parameter
-    this.openNewAppointment('0ms', '0ms',event);
+  teste(teste:Date|null){
+    console.log(teste)
+  }
+  generateHoursAMPM() {
+    for (let i = 0; i < 24; i++) {
+      const hour = i % 12 || 12;
+      const suffix = i < 12 ? 'AM' : 'PM';
+      this.hoursAMPM.push(hour + ' ' + suffix);
+    }
+  }
+  selectHour(hour: string) {
+    console.log(hour)
+    this.selectedHour = hour;
+    this.openNewAppointment('0ms', '0ms',hour);
+
+  }
+  isHourSelected(hour: string) {
+    
+    return this.selectedHour === hour;
   }
 }
